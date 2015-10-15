9254aecf687decf991c2d5b691b53959df3281b2
manzanares-randy <manzanares.randy@gmail.com>
2015-10-15 13:20:16 -0700

    add

--- Lab 2/List/ListStack.cpp
+++ Lab 2/List/ListStack.cpp
@@ -1,1 +1,32 @@
 #include "List.h"
+template <class T>
+
+T List<T>:: getHeadData()
+{
+
+	return head->info;
+
+}
+List:: addFront(T data)
+{
+
+	if (head == NULL) // case of empty list
+    {
+      	head = new Data();
+        head -> info = data ;
+		head -> next = NULL;
+    }
+    else {
+
+	    Data* newnode = new Data();
+		newnode -> info = data ;
+		newnode -> next = head;
+		head = newnode;
+    }
+
+}
+List:: delFront()
+{
+
+
+}
