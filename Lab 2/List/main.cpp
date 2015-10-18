5feea64cb567bc54e768313507fa48b686db36d9
WangWillis <willis257@gmail.com>
2015-10-18 13:15:41 -0700

    Main tester with try catch block

--- Lab 2/List/main.cpp
+++ Lab 2/List/main.cpp
@@ -15,15 +15,20 @@ int main() {
     // list.addFront(1);
     // list.addFront(23);
     // list.displayList();
-    
     List <string> list2;
     list2.addFront("Randy");
     list2.delInfo("Randy");
     list2.displayList();
     list2.addFront("willis");
-    list2.addFront("Randy");
-    list2.addInfoOrder("willis");
+    list2.addFront("randy");
+    list2.addInfoOrder("Willis");
     list2.addInfoOrder("Randy");
+    try{
+        list2.delPos(4);        
+    } catch(const char* exc){
+        std::cout << exc << std::endl;
+    }
+
     list2.displayList();
     
     
