8554545eecfd45263ca2ebc3e2b2df9423028546
WangWillis <willis257@gmail.com>
2015-10-30 17:52:12 -0700

    worked more on the program

--- Lab 3/main.cpp
+++ Lab 3/main.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <string>
+#include <locale>
 #include "StackList.h"
 #include "Queue.h"
 
@@ -9,11 +10,37 @@ int main(){
 	string eqn;
 	Stack <char> s1;
 	Stack <char> s2;
-	Queue <char> q1;
+	Queue <char> postFixStream;
+	//for checking to see if equation has invalid characters
+	bool isGood = true;
 	cout << "Enter your equation" << endl;
 	getline(cin, eqn);
+	//removes all whitespace from the string and checks to see if the string has invalid characters
 	for(int i = 0; i < eqn.size(); i++){
-		
+		if(eqn[i] == ' '){
+			eqn.erase(i, 1);
+			i -= 1;
+		}else if(!isdigit(eqn[i]) && eqn[i] != '+' && eqn[i] != '-' && eqn[i] != '(' && eqn[i] != ')' && eqn[i] != '*' && eqn[i] != '/'){
+			isGood = false;
+			break;
+		}
+	}
+	if(isGood && (isdigit(eqn[0]) || eqn[0] == '(')){
+		for(int i = 0; i < eqn.size(); i++){
+			if(eqn[i] == '(' || eqn[i] == ')' || eqn[i] == '+' || eqn[i] == '-' || eqn[i] == '*' || eqn[i] == '/'){
+				char temp = eqn[i];
+			}else if(isdigit(eqn[i])){
+				postFixStream.add(eqn[i]);
+			}else{
+				cout << "Invalid Equation" << endl;
+				break;
+			}
+		}
+	}else{
+		cout << "Invalid Equation" << endl;
+	}
+	if(!s2.isEmpty()){
+		cout << "Invalid Equation" << endl;
 	}
 	return 0;
 }
