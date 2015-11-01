6b83d49c9c29ea60210a06c2f4ce6cd450b42660
WangWillis <willis257@gmail.com>
2015-10-31 19:02:38 -0700

    layed out invalid eqn checks possible when not evaluating

--- Lab 3/main.cpp
+++ Lab 3/main.cpp
@@ -9,7 +9,7 @@ using namespace std;
 int main(){
 	string eqn;
 	Stack <char> s1;
-	Stack <char> s2;
+	Stack <int> s2;
 	Queue <char> postFixStream;
 	//for checking to see if equation has invalid characters
 	bool isGood = true;
@@ -27,20 +27,43 @@ int main(){
 	}
 	if(isGood && (isdigit(eqn[0]) || eqn[0] == '(')){
 		for(int i = 0; i < eqn.size(); i++){
-			if(eqn[i] == '(' || eqn[i] == ')' || eqn[i] == '+' || eqn[i] == '-' || eqn[i] == '*' || eqn[i] == '/'){
+			if(eqn[i] == '('){
 				char temp = eqn[i];
+				s1.push(eqn[i]);
+			}else if(eqn[i] == ')' && !s1.isEmpty()){
+				while(s1.get() != '('){
+					postFixStream.add(s1.pop());
+					if(s1.isEmpty()){
+						isGood = false;
+						break;
+					}
+				}
+				if(isGood){
+					//gets rid of the (
+					s1.pop();
+				}else{
+					isGood = false;
+					cout << "Invalid Equation" << endl;
+					break;
+				}
+			}else if(eqn[i] == '+' || eqn[i] == '-' ){
+
+			}else if(eqn[i] == '*' || eqn[i] == '/'){
+				
 			}else if(isdigit(eqn[i])){
 				postFixStream.add(eqn[i]);
 			}else{
+				isGood = false;
 				cout << "Invalid Equation" << endl;
 				break;
 			}
 		}
 	}else{
+		isGood = false;
 		cout << "Invalid Equation" << endl;
 	}
-	if(!s2.isEmpty()){
-		cout << "Invalid Equation" << endl;
-	}
+	// if(!s2.isEmpty()){
+	// 	cout << "Invalid Equation" << endl;
+	// }
 	return 0;
 }
