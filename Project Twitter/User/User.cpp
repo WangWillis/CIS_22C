45bff13c36fc39da77d37cec58c4db4b7fc2631a
PatriciaCR <patriciacr219@gmail.com>
2015-11-11 00:50:43 -0800

    Setters and getters
    BST needs to be completed to write the other stuff

---
+++ Project Twitter/User/User.cpp
@@ -0,0 +1,62 @@
+#include "User.h"
+#include "TweetContainer.h"
+
+User::User(string username, string psswrd)
+{
+	userName = username;
+	password = psswrd;
+	followers = 0;
+	following = 0;
+}
+
+void User::setUsername(string username)
+{
+	userName = username;
+}
+
+void User::setPassword(string psswrd)
+{
+	password = psswrd;
+}
+
+void User::setFollowers(int followrs)
+{
+	followers = followrs;
+}
+
+void User::setFollowing(int followng)
+{
+	following = followng;
+}
+
+string User::getUsername()
+{
+	return userName;
+}
+
+string User::getPassword()
+{
+	return password;
+}
+
+int User::getFollowers()
+{
+	return followers;
+}
+int User::getFollowing()
+{
+	return following;
+}
+
+void User::addTweet(Tweet* pst)
+{
+	MyTweet Tweeting;
+	Tweeting.setPost(pst);
+	myTweets.addFront(Tweeting);
+}
+
+void User::deleteTweet (MyTweet pst)
+{
+	myTweets.delInfo(pst);
+}
+
