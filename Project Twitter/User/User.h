4299b5de32f47eb8c06c3932420f9fe621ceef7a
PatriciaCR <patriciacr219@gmail.com>
2015-11-11 00:51:54 -0800

    Still needs more member functions

--- Project Twitter/User/User.h
+++ Project Twitter/User/User.h
@@ -1,5 +1,6 @@
 #include "BST.h"
 #include "List.h"
+#include "TweetContainer.h"
 #include "Tweet.h"
 #include <string>
 
@@ -8,9 +9,36 @@ using namespace std;
 class User;
 
 class User{
-	private:
-		string userName, password;
-	public:
-		User();
-		~User();
+private:
+	string userName, password;
+	int followers, following;
+	List <MyTweet> myTweets;
+	BinarySearchTree <UserTweet> NewsFeed;
+	BinarySearchTree <string> Followers;
+	BinarySearchTree <string> Following;
+
+public:
+	User();
+	~User();
+	User(string, string);
+
+	void setUsername(string);
+	void setPassword(string);
+	void setFollowers(int);
+	void setFollowing(int);
+
+	string getUsername();
+	string getPassword();
+	int getFollowers();
+	int getFollowing();
+
+
+	void addTweet(Tweet*); //Needs to be completed
+	void deleteTweet(MyTweet); //Delete by position or tweet?
+
+	//BinarySearchTree needs to be completed
+	void displayFollowers();
+	void displayFollowing();
+	void displayNewsFeed();
+	void toQueue();
 };
