6e68d55e7a502eb81a7f98de3ac05b046139cc43
PatriciaCR <patriciacr219@gmail.com>
2015-11-12 00:46:53 -0800

    Update User.h

--- Project Twitter/User/User.h
+++ Project Twitter/User/User.h
@@ -14,9 +14,10 @@ private:
 	string userName, password;
 	int numFollower, numFollowing;
 	List <MyTweet> myTweets;
-//	AVLTree <UserTweet> newsFeed;
-//	AVLTree <string> followers;
-//	AVLTree <string> following;
+	List <MyTweet> myRetweets;
+	AVLTree <MyTweet> newsFeed;
+	AVLTree <string> followers;
+	AVLTree <string> following;
 
 public:
 	User();
@@ -32,16 +33,12 @@ public:
 	bool matchPassword(string);
 	int getFollowers();
 	int getFollowing();
+	
+	void addTweet(Tweet*);
+	void deleteTweet(MyTweet); 
 
-	//this just general add so just add a new tweet container to the newsfeed
-	//Looks like the hash table needs to be completed for this :)
-	void addTweet(Tweet*); //Needs to be completed
-	void deleteTweet(MyTweet); //Delete by tweet also need to delete from followers newsfeed but prob not in main
-
-	//Tree has been completed
-	//BinarySearchTree needs to be completed
 	void displayFollowers();
 	void displayFollowing();
 	void displayNewsFeed();
-	Queue toQueue();
+	Queue <string> toQueuefollowers();
 };
