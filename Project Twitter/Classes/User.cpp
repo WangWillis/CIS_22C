3fea43e930ca038834462ed76550a11f11c7d044
WangWillis <willis257@gmail.com>
2015-11-14 21:55:58 -0800

    updated dir look and added my test file

---
+++ Project Twitter/Classes/User.cpp
@@ -0,0 +1,122 @@
+#include "User.h"
+
+User::User(string username, string psswrd)
+{
+	userName = username;
+	password = psswrd;
+	numFollower = 0;
+	numFollowing = 0;
+}
+
+void User::setUsername(string username)
+{
+	userName = username;
+}
+
+void User::setPassword(string psswrd)
+{
+	password = psswrd;
+}
+
+void User::setFollowers(int followrs)
+{
+	numFollower = followrs;
+}
+
+void User::setFollowing(int followng)
+{
+	numFollowing = followng;
+}
+
+void User::setNumTweets(int Ntweets)
+{
+	numTweets = Ntweets;
+}
+
+void User::setNumRTweets(int RT)
+{
+	numRetweets = RT;
+}
+
+void User::setNumNewsF(int NF)
+{
+	numTNewsF = NF;
+}
+
+int User::getNumTweets()
+{
+	return numTweets;
+}
+int User::getNumRTweets()
+{
+	return numRetweets;
+}
+int User::getNumNewsF()
+{
+	return numTNewsF;
+}
+
+string User::getUsername()
+{
+	return userName;
+}
+
+bool User::matchPassword(string pass)
+{
+	if (password == pass)
+		return true;
+	else false;
+}
+
+int User::getFollowers()
+{
+	return numFollower;
+}
+int User::getFollowing()
+{
+	return numFollowing;
+}
+
+void User::addTweet(Tweet* pst)
+{
+	if (pst->getUserId() == userName){
+		MyTweet Tweeting(pst);
+		myTweets.addFront(Tweeting);
+	}
+	UserTweet newsTweet(pst);
+	newsFeed.add(newsTweet);
+}
+
+void User::deleteTweet(UserTweet pst)
+{
+	if (pst.getTweet()->getUserId() == userName){
+		if (pst.isReTweet() == true)
+			myRetweets.delInfo(pst);
+		else
+			myTweets.delInfo(pst);
+	}
+	newsFeed.remove(pst);
+}
+
+void User::displayFollowers()
+{
+	followers.toString();
+}
+void User::displayFollowing()
+{
+	following.toString();
+}
+void User::displayNewsFeed()
+{
+	newsFeed.toString();
+}
+
+//Since the user class does not interact with the hash table
+//to remove and add tweets to users who are following you need to be done in main
+//in order to do that we need to stream the people who are following you
+//input those string into the has table and add or remove the tweet from their newsfeed
+Queue<string> User::toQueueFollowers()
+{
+	return followers.toQueue();
+}
+
--- Project Twitter/test.cpp
+++ Project Twitter/test.cpp
@@ -1,35 +1,16 @@
 #include <iostream>
-#include "Tweet\Tweet.h"
-#include "Tweet\TweetContainer.h"
-#include "BinaryTree\BTNode.h"
+#include "Classes\Tweet\Tweet.h"
+#include "Classes\Tweet\TweetContainer.h"
+#include "Classes\Data Structures\Hash.h"
+#include <string>
 
 using namespace std;
 
 int main(){
-	// Tweet* t1 = new Tweet("WILLYBEAR", "SUP WORLD");
-	// string s1, s2;
-	// getline(cin, s1);
-	// getline(cin, s2);
-	// Tweet* r1 = new ReTweet(t1, s1, s2);
-	// MyTweet m1(t1);
-	// MyTweet m2(r1);
-	// m1.toString();
-	// m2.toString();
-	BTNode<int>* head = NULL;
-	for(int i = 0; i < 5; i++){
-		int temp;
-		cin >> temp;
-		head = head->add(temp, head);
-	}
-	cout << endl;
-	head->toString(head);
-	cout << endl;
-	// int temp;
-	// cin >> temp;
-	// cout << endl;
-	// delete head->remove(temp, head);
-	// head->toString(head);
-	head->clearList(head);
-	//delete head;
+	HashTable<int*> hash1;
+	hash1.addItem("SUP", new int [1]);
+	int* temp = hash1.getData("SUP");
+	temp[0] = 1;
+	cout << temp[0] << endl;
 	return 0;	
 }
