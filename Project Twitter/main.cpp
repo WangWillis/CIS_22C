95e92a7aa577622e58a70980220e1223dbd0249f
Jason-Do <jason.do2311@gmail.com>
2015-11-29 01:41:54 -0800

    Made main look better (not done yet) and found several crashes

--- Project Twitter/main.cpp
+++ Project Twitter/main.cpp
@@ -18,15 +18,17 @@ int main(){
 	cout << "   | $$| $$ | $$ | $$| $$  | $$ /$$| $$ /$$| $$_____/| $$         .-'`_.- '``-..,,;;;;:'      " << endl;
 	cout << "   | $$|  $$$$$/$$$$/| $$  |  $$$$/|  $$$$/|  $$$$$$$| $$         `''`           `\\`\\       " << endl;
 	cout << "   |__/ \\_____/\\___/ |__/   \\___/   \\___/   \\_______/|__/                         /^\\\\\\    " << endl << endl;
-	cout << string(94, '=') << endl << endl;
+	cout << string(94, '=') << endl;
 	int choice;
 	do{
+		cout << endl << "Welcome to Twitter!  Please enter a number to indicate what you wish to do." << endl;
 		cout << "1. Login" << endl;
 		cout << "2. Register" << endl;
 		cout << "3. Exit" << endl;
 		cin >> choice;
+		cin.clear();
+		cin.ignore(numeric_limits <streamsize> ::max(), '\n');
 		if(choice == 1){
-			cin.ignore();
 			string username, pass;
 			cout << "Please enter your username" << endl;
 			getline(cin, username);
@@ -35,123 +37,132 @@ int main(){
 			User* user = helper.getUser(username, pass);
 			if(user != NULL){
 				int userChoice;
-				cout << "Welcome " << user->getUsername() << endl;
+				cout << endl << "Welcome " << user->getUsername() << "!" << endl;
 				do{
+					cout << endl << "What would you like to do?  Please enter a valid choice." << endl;
 					cout << "1. Add Tweet" << endl;
 					cout << "2. View Newsfeed" << endl;
 					cout << "3. View My Profile" << endl;
-					cout << "4. Display all users" << endl;
+					cout << "4. Display all Users" << endl;
 					cout << "5. Follow User" << endl;
 					cout << "6. Logout" << endl;
 					cin >> userChoice;
+					cin.clear();
+					cin.ignore(numeric_limits <streamsize> ::max(), '\n');
 					if(userChoice == 1){
-						cin.ignore();
 						string msg;
+						cout << endl <<"What's on your mind?" << endl;
 						cout << "Tweet Msg: " << endl;
 						getline(cin, msg);
-						helper.add(user, msg);
+						helper.add(user, msg); //crashes here
 					}else if(userChoice == 2){
+						cout << endl;
 						user->displayNewsFeed();
 					}else if(userChoice == 3){
 						int pChoice;
 						do{
+							cout << endl << "What would you like to do?  Please enter a valid choice." << endl;
 							cout << "1. View My Tweets" << endl;
 							cout << "2. Delete a Tweet" << endl;
 							cout << "3. View My Followers" << endl;
 							cout << "4. View who I am Following" << endl;
 							cout << "5. Back" << endl;
 							cin >> pChoice;
+							cin.clear();
+							cin.ignore(numeric_limits <streamsize> ::max(), '\n');
 							if(pChoice == 1){
+								cout << endl;
 								user->displayMyTweets();
 							}else if(pChoice == 2){
 								Queue<MyTweet> myTweetStream = user->toQueueMyTweet();
 								int i = 1;
 								int option;
-								if(!myTweetStream.isEmpty()){
+								cout << "hip" << endl;
+								if(!myTweetStream.isEmpty()){ //crashes here
+									cout << endl;
 									while(!myTweetStream.isEmpty()){
 										cout << i << ". " << myTweetStream.pop() << endl;
 										i++;
 									}
-									cout << "Please select the Tweet you would like to delete by number" << endl;
+									cout << "Please select the Tweet you would like to delete by entering its number." << endl;
 									cin >> option;
+									cin.clear();
+									cin.ignore(numeric_limits <streamsize> ::max(), '\n');
 									if(option <= i && option >= 1){
 										MyTweet temp = user->getMyTweet(option-1);
 										helper.remove(user, temp);
-										cout << "Deleted" << endl;
+										cout << "The Tweet has been successfully deleted" << endl;
 									}else{
-										cin.clear();
-										cin.ignore();
-										cout << "Invalid Input" << endl;
+										cout << "I'm sorry, that's an invalid input!" << endl;
 									}
 								}else{
-									cout << "No Tweets to Delete" << endl;
+									cout << "There are currently no tweets to delete." << endl;
 								}
 							}else if(pChoice == 3){
-								cout << "Number of Followers: " << user->getFollowers() << endl;
+								cout << endl << "Number of Followers: " << user->getFollowers() << endl;
 								user->displayFollowers();
 							}else if(pChoice == 4){
-								cout << "Number of People I Follow " << user->getFollowing() << endl;
+								cout << endl << "Number of People I Follow: " << user->getFollowing() << endl;
 								user->displayFollowing();
 							}else if(pChoice != 5){
-								cin.clear();
-								cin.ignore();
-								cout << "Invalid input" << endl;
+								cout << "I'm sorry, that's an invalid input!" << endl;
 							}
 						}while(pChoice != 5);
 					}else if(userChoice == 4){
+						cout << endl;
 						helper.displayUsers();
 					}else if(userChoice == 5){
-						cin.ignore();
 						string temp;
-						cout << "Enter the username of the person you would like to follow" << endl;
+						cout << endl << "Please enter the username of the person you would like to follow." << endl;
 						getline(cin, temp);
 						if(!helper.checkKey(temp)){
-							helper.follow(user, temp);
-							cout << "You are now following " << temp << endl;
+							cout << "hip" << endl;
+							helper.follow(user, temp);  //crashes here
+							cout << "You are now following " << temp << "!" << endl;
 						}else{
-							cout << temp << " is not a valid user" << endl;
+							cout << temp << " is not a valid user!" << endl;
 						}
 					}else if(userChoice == 6){
-						cout << "Goodbye " << user->getUsername() << endl;
+						cout << "Goodbye!" << user->getUsername() << endl;
 						user = NULL;
 					}else{
-						cin.clear();
-						cin.ignore();
-						cout << "Invalid input" << endl;
+						cout << "I'm sorry, that's an invalid input!" << endl;
 					}
 				}while(userChoice != 6);
 			}else{
-				cout << "Invalid Username and/or Password" << endl;
+				cout << "Invalid Username and/or Password!" << endl;
 			}
 		}else if(choice == 2){
-			cin.ignore();
-			string username, pass;
+			string username, pass, test;
 			bool isGood = false;
 			do{
-				cout << "Enter your desired username. Type exit to exit this process" << endl;
+				cout << endl << "Enter your desired username. Or type 'exit' to exit this process." << endl;
 				cout << "Your username cannot be user" << endl;
 				getline(cin, username);
+				test = username;
+				for (int i = 0; i < test.length(); i++)
+				{
+					test[i] = tolower(test[i]);
+				}
 				isGood = helper.checkKey(username);
-				if((!isGood || username == "user") && (username != "exit" || username != "Exit")){
+				if ((!isGood || test == "user") && test != "exit"){
 					cout << "Invalid User name" << endl;
 				}
-			}while((username != "exit" || username != "Exit") && (!isGood || username == "user"));
-			if((username != "exit" && username != "Exit")){
-				cout << "Please enter your desired password" << endl;
+			}while((test != "exit") && (!isGood || test == "user"));
+			if(test != "exit"){
+				cout << "Please enter your desired password." << endl;
 				getline(cin, pass);
 				//can put a make sure for password here
 				User* user = new User(username, pass);
 				helper.addUser(user);
-				cout << "New user created" << endl;
+				cout << "Your account has successfully been created!" << endl << endl;
 			}
 		}else if(choice == 3){
-			cout << "Goodbye" << endl;
+			cout << "Goodbye!" << endl << endl;
 		}else{
-			cin.ignore();
-			cin.clear();
-			cin.ignore();
-			cout << "Invalid input" << endl;
+			cout << "I'm sorry, that's an invalid input!" << endl << endl;
 		}
 	}while(choice != 3);
-	return 0;
+	cout << "hoop" << endl;
+	return 0; //crashes if a user was created.  User destructor must be broken
 }
